2d76466ea3428b8d6f1144f509674a6fb91dc0cf
James <JamesCarlson2@hotmail.com>
2015-10-05 11:17:48 -0500

    Complete checkpoint 29 assignment

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe WelcomeController, type: :controller do #could also be written :t
       expect(response).to render_template("about")
     end
   end
+
+  describe "GET faq" do
+    it "renders the faq template"do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
 end
