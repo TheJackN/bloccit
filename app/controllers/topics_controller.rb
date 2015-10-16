8e89560113c95fa6fcaab16a9d6183f8d03ae2a8
James <JamesCarlson2@hotmail.com>
2015-10-16 13:19:22 -0500

    Complete Checkpoint 43/40 assignment

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,8 @@
 class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show]
+  before_action :moderator_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show, :edit, :update]
 
   def index
     @topics = Topic.all
@@ -59,6 +60,13 @@ class TopicsController < ApplicationController
 
   def topic_params
     params.require(:topic).permit(:name, :description, :public)
+  end
+
+  def moderator_user
+    unless current_user.moderator? || current_user.admin?
+      flash[:error] = "You must be an admin or moderator to do that"
+      redirect_to topics_path
+    end
   end
 
   def authorize_user
